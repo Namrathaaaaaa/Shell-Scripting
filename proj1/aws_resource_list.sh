d1d7d037827903621589fa391af5d72d59a1db4b
Namratha <namratha343@gmail.com>
2025-09-21 11:20:40 +0530

    proj1 documentation

--- proj1/aws_resource_list.sh
+++ proj1/aws_resource_list.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-<< task
-Building a script that can list all the resources in aws account
-Authors: Devops team
-task
+# This script will list all the resources in the AWS account 
+# Author: Namratha/Devops
+# version: v0.0.1
 
+
+#########################################################################################
+# Following are the supported AWS services by the script 
+# 1.EC2
+# 2.S3
+# 3.DynamoDB
+# 4.Lambda
+# 5.RDS
+# 6.VPC
+# 7.CloudFront
+# 8.EBS
+# 9.ELB
+# 10.SNS
+# 11.SQS
+# 12.CloudWatch
+# 13.Route53
+# 14.IAM
+# 15.CloudFormation
+
+# Usage: ./aws_resource_list.sh <region> <service_name>
+# Example: ./aws_resource_list.sh us-east-1 ec2
+###########################################################################################
